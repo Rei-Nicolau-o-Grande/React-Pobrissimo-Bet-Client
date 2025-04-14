252853aa573d8c28a409618faf9de3aa6ecbb117
Jack Sparrow <nicolasmeireles1004@gmail.com>
2025-04-14 19:18:31 -0300

    :poop: feat: add .env

--- src/helper/axios-instance.js
+++ src/helper/axios-instance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: `https://${apiUrl}:8080/api/v1`,
     headers: {
         "Content-type": "application/json",
     }
